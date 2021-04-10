b2892cb270912a43f1da3f825d9a78aef99e5c01
Andrey1771 <Hohlov908@gmail.com>
2021-04-10 18:28:10 +0300

    update front, add LoginPage/RegistrationPage, add Registration (not finished), fix images card svg

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -6,7 +6,10 @@ using OnlineShopDuhootWeb.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using System.Net;
 
 namespace OnlineShopDuhootWeb.Controllers
 {
@@ -58,5 +61,78 @@ namespace OnlineShopDuhootWeb.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new OnlineShopDuhootWebUser() { /*UserName = model.UserName*/ };
+                user.Email = model.Email;
+                user.EmailConfirmed = false;
+                var result = await userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    // наш email с заголовком письма
+                    MailAddress from = new MailAddress("somemail@yandex.ru", "Web Registration");
+                    // кому отправляем
+                    MailAddress to = new MailAddress(user.Email);
+                    // создаем объект сообщения
+                    MailMessage m = new MailMessage(from, to);
+                    // тема письма
+                    m.Subject = "Email confirmation";
+                    // текст письма - включаем в него ссылку
+                    m.Body = string.Format("Для завершения регистрации перейдите по ссылке:" +
+                                    "<a href=\"{0}\" title=\"Подтвердить регистрацию\">{0}</a>",
+                        Url.Action("ConfirmEmail", "Account", new { Token = user.Id, Email = user.Email }, Request.Scheme/*Request.Url.Scheme*/));
+                    m.IsBodyHtml = true;
+                    // адрес smtp-сервера, с которого мы и будем отправлять письмо
+                    SmtpClient smtp = new SmtpClient("smtp.yandex.ru", 25);
+                    // логин и пароль
+                    smtp.Credentials = new NetworkCredential("somemail@yandex.ru", "password");
+                    smtp.Send(m);
+                    return RedirectToAction("Confirm", "Account", new { Email = user.Email });
+                }
+                else
+                {
+                    //AddErrors(result);
+                }
+            }
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        public string Confirm(string Email)
+        {
+            return "На почтовый адрес " + Email + " Вам высланы дальнейшие" +
+                    "инструкции по завершению регистрации";
+        }
+
+        [AllowAnonymous]
+        public async Task<ActionResult> ConfirmEmail(string Token, string Email)
+        {
+            OnlineShopDuhootWebUser user = await userManager.FindByIdAsync(Token);
+            if (user != null)
+            {
+                if (user.Email == Email)
+                {
+                    user.EmailConfirmed = true;
+                    await userManager.UpdateAsync(user);
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home", new { ConfirmedEmail = user.Email });
+                }
+                else
+                {
+                    return RedirectToAction("Confirm", "Account", new { Email = user.Email });
+                }
+            }
+            else
+            {
+                return RedirectToAction("Confirm", "Account", new { Email = "" });
+            }
+        }
     }
 }
---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopDuhootWeb.Models
+{
+
+    public enum GenderType { 
+        Man, Woman
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Text)]
+        public string SecondName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateBirth { get; set; }
+
+        public GenderType Gender { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [DataType(DataType.Password)]
+        public string PasswordConfirm { get; set; }
+
+
+    }
+}
